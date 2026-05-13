89a35cf68220bcf62cf5653bf0f27304cb976bfb
= <=>
2026-05-13 13:51:26 -0400

    Buncha Stuff with certifications.html

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 //This may work outside of iFrames but could lead to bad displays
 function toggleVisibleIFrame(elementID) {
     var element = document.getElementById(elementID);
-    if (element.style.display == 'block') {
-        element.style.display = 'none';
-    } else {
-        element.style.display = 'block';
+    element.style.display = element.style.display === 'none' ? 'block' : 'none';
+}
+
+
+function runOnLoadThingy() {
+    const linkHash = (window.location.hash).substring(1,(window.location.hash).length);
+    const ids = Array.from(document.getElementsByTagName('object')).map(obj => obj.id);
+
+    for (var i=0;i<ids.length;i++) {
+        var tempElement = document.getElementById(ids[i])
+        tempElement.style.display = ids[i] == linkHash ? 'block' : 'none'
     }
 }
+
+window.addEventListener('DOMContentLoaded', runOnLoadThingy)
+window.addEventListener('hashchange', runOnLoadThingy);
